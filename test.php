97db3a64b1e8265b43712cdfe3b4f1cc5cb531df
don1291 <32994255+don1291@users.noreply.github.com>
2018-11-28 23:37:36 -0500

    Add files via upload

---
+++ test.php
@@ -0,0 +1,80 @@
+{
+	"pies":  
+	[
+		{
+			"id": "1",
+			"Flavor": "Pumpkin",
+			"Slice": "4.50",
+			"Whole": "12.00"
+		},
+		{
+			"id": "2",
+			"Flavor": "Blueberry",
+			"Slice": "3.50",
+			"Whole": "12.50"
+		},
+		{
+			"id": "3",
+			"Flavor": "Apple",
+			"Slice": "3.50",
+			"Whole": "12.50"
+		},
+		{
+			"id": "4",
+			"Flavor": "Pumpkin",
+			"Slice": "3.50",
+			"Whole": "12.50"
+		}
+	],
+	"cheesecakes":[
+		{
+			"id": "4",
+			"Flavor": "Chocolate",
+			"Slice": "2.75",
+			"Whole": "12.50"
+		},
+		{
+			"id": "5",
+			"Flavor": "Blueberry",
+			"Slice": "2.75",
+			"Whole": "12:50"
+		},
+		{
+			"id": "6",
+			"Flavor": "Strawberry",
+			"Slice": "2.75",
+			"Whole": "12.50"
+		},
+		{
+			"id": "7",
+			"Flavor": "Pumpkin",
+			"Slice": "3.50",
+			"Whole": "12.50"
+		}
+	],
+	"italian":
+	[
+		{
+			"id": "8",
+			"Flavor": "Cannoli",
+			"Price": "4.00"
+		},
+		{
+			"id": "9",
+			"Flavor": "Eclairs",
+			"Price": "3.75"
+		},
+		{
+			"id": "10",
+			"Flavor": "Napoleon",
+			"Price": "3.75"
+		},
+		{
+			"id": "11",
+			"Flavor": "Creme Puff",
+			"Price": "3.75"
+		}
+
+	]
+	
+}
